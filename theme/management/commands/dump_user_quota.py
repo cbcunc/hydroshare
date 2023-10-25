f73457d383b1cccddd562b4f40dea69ddc6c5e98
Devin Cowan <dcowan@cuahsi.org>
2023-10-25 16:34:47 -0400

    dump resource quota U/D breakdown
    (cherry picked from commit fcea47281f2aa6a72e0da8e19d21211acfeca8a6)

--- theme/management/commands/dump_user_quota.py
+++ theme/management/commands/dump_user_quota.py
@@ -1,6 +1,8 @@
 import csv
 from django.core.management.base import BaseCommand
 from theme.models import UserQuota
+from hs_core.hydroshare import get_data_zone_usage_from_irods, get_user_zone_usage_from_irods
+from hs_core.hydroshare.utils import convert_file_size_to_unit
 
 
 class Command(BaseCommand):
@@ -18,18 +20,26 @@ class Command(BaseCommand):
                 'Allocated quota value',
                 'Used quota value',
                 'Quota unit',
-                'Storage zone'
+                'Storage zone',
+                'Data Zone used',
+                'User Zone used'
             ]
             w.writerow(fields)
 
             for uq in UserQuota.objects.filter(
                     user__is_active=True).filter(user__is_superuser=False):
+                data_zone = get_data_zone_usage_from_irods(uq.user.username)
+                user_zone = get_user_zone_usage_from_irods(uq.user.username)
+                data_zone = convert_file_size_to_unit(data_zone, 'GB')
+                user_zone = convert_file_size_to_unit(user_zone, 'GB')
                 values = [
                     uq.user.id,
                     uq.user.username,
                     uq.allocated_value,
                     uq.used_value,
                     uq.unit,
-                    uq.zone
+                    uq.zone,
+                    data_zone,
+                    user_zone
                 ]
                 w.writerow([str(v) for v in values])
